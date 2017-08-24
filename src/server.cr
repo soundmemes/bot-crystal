ab90acdb3d1fc51895f0936f3e5331416948a9a6
Vlad Faust <vladislav.faust@gmail.com>
2017-08-24 17:53:44 +0300

    ref: better server.cr

--- src/server.cr
+++ src/server.cr
@@ -5,22 +5,26 @@ require "../initializers/dispatch/schedule_jobs"
 require "../soundmemes/telegram_bot/bot"
 require "../soundmemes/version"
 
-logger = Logger.new(STDOUT).tap { |l| l.level = Utils::Logger.logger_level }
-logger.info "Launching Soundmemes version #{Soundmemes::VERSION}"
+class Server
+  include Utils::Logger
 
-begin
-  schedule_jobs
+  def initialize(bot_api_token token, host, port, @webhook_host : URI)
+    @bot = Soundmemes::TelegramBot::Bot.new(token, port, logger, host: host)
+  end
 
-  uri = URI.new(scheme: "https", host: ENV["BOT_WEBHOOK_HOST"])
-
-  bot = Soundmemes::TelegramBot::Bot.new(
-    token: ENV["BOT_API_TOKEN"],
-    host: "0.0.0.0",
-    port: ENV["PORT"].to_i,
-    logger: logger,
-  )
-  bot.set_webhook(uri, allowed_updates: %w(message inline_query chosen_inline_result callback_query))
-  bot.listen
-ensure
-  db.close
+  def run
+    logger.info "Running Soundmemes Bot version %s" % Soundmemes::VERSION
+    @bot.set_webhook(@webhook_host, allowed_updates: %w(message inline_query chosen_inline_result callback_query))
+    @bot.listen
+  end
 end
+
+server = Server.new(
+  bot_api_token: ENV["BOT_API_TOKEN"],
+  host: "0.0.0.0",
+  port: ENV["PORT"].to_i,
+  webhook_host: URI.new(scheme: "https", host: ENV["BOT_WEBHOOK_HOST"]),
+)
+
+schedule_jobs
+server.run
