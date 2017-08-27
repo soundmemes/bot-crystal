ab28b7b70b56a8d33504023ae8a44656144d9cac
Vlad Faust <vladislav.faust@gmail.com>
2017-08-27 15:34:13 +0300

    ref: Utils::Logger

--- src/utils/logger.cr
+++ src/utils/logger.cr
@@ -1,17 +1,9 @@
 require "logger"
 
 module Utils::Logger
-  getter logger : ::Logger do
-    ::Logger.new(STDOUT).tap do |l|
-      l.level = ENV["APP_ENV"] == "production" ? ::Logger::INFO : ::Logger::DEBUG
-    end
-  end
+  getter logger : ::Logger { ::Logger.new(STDOUT).tap &.level = logger_level }
 
-  class_getter logger : ::Logger do
-    ::Logger.new(STDOUT).tap { |l| l.level = logger_level }
-  end
-
-  def self.logger_level
+  def logger_level
     ENV["APP_ENV"] == "production" ? ::Logger::INFO : ::Logger::DEBUG
   end
 end
